bf4884a3254f769f2917557d3db2f487223bfb0e
sonoda-kengo <wsho.magn07@gmail.com>
2022-12-02 11:30:56 +0900

    fix: formatting of eol

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
+import useTranslation from 'next-translate/useTranslation';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import useTranslation from 'next-translate/useTranslation';
 
 export default function Home() {
   const { t } = useTranslation();
