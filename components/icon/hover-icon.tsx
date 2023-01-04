a8886d7e1c2ba208003128d5bad9473d9452c4ec
sonoda-kengo <wsho.magn07@gmail.com>
2023-01-04 09:22:10 +0900

    change hover-icon's Box to Grid item

--- components/icon/hover-icon.tsx
+++ components/icon/hover-icon.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import Image from 'next/image';
 import { useState } from 'react';
 
@@ -12,7 +12,8 @@ export const HoverIcon = ({ src, title, subTitle }: IHoverIcon) => {
   const [isDisplay, setIsDisplay] = useState(false);
 
   return (
-    <Box
+    <Grid
+      item
       margin={2}
       onMouseOver={() => setIsDisplay(true)}
       onMouseOut={() => setIsDisplay(false)}
@@ -22,11 +23,11 @@ export const HoverIcon = ({ src, title, subTitle }: IHoverIcon) => {
     >
       <Image src={src} alt={title} width={70} height={70} />
       {isDisplay && title && (
-        <Box marginLeft='0.8rem'>
+        <Grid item marginLeft='0.8rem'>
           <Typography color='textSecondary'>{title}</Typography>
           <Typography color='textSecondary'>{subTitle}</Typography>
-        </Box>
+        </Grid>
       )}
-    </Box>
+    </Grid>
   );
 };
