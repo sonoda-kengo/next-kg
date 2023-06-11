a8f711812e4ff44fb93e4206de34e09e0008f2bf
sonoda-kengo <wsho.magn07@gmail.com>
2023-06-11 09:16:29 +0900

    modify contact's image

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,23 +1,27 @@
-import { Container, Grid, List } from '@mui/material';
+import { Container, Grid, List, useMediaQuery, useTheme } from '@mui/material';
 import Image from 'next/image';
 import Layout from 'components/layout';
 import { RowListItem } from 'components/list/row-list-item';
 
 export default function Contact() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const imageWidth = isMobile ? 200 : 300;
+  const imageHeight = isMobile ? 280 : 400;
   return (
     <Layout>
       <Container>
         <h1>Contact</h1>
         <Grid container direction='row' alignContent='center' justifyContent='center'>
-          <Grid item xs={6}>
+          <Grid item xs={12} md={6}>
             <List>
               <RowListItem itemKey='name' itemValue='Sonoda Kengo' />
               <RowListItem itemKey='adress' itemValue='Setagaya, Tokyo' />
               <RowListItem itemKey='e-mail' itemValue='kengob6@gmail.com' />
             </List>
           </Grid>
-          <Grid item xs={6}>
-            <Image src='/images/me.jpg' alt='my photo' width={300} height={400} />
+          <Grid item xs={12} md={6} display='contents'>
+            <Image src='/images/me.jpg' alt='my photo' width={imageWidth} height={imageHeight} />
           </Grid>
         </Grid>
       </Container>
